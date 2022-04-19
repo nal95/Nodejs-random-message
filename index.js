e8d7a08b45b51b96546a6eaa5bf55ab466666d76
nal95 <alex.nguepi@yahoo.fr>
2022-04-19 06:18:45 +0200

    random PackstueckNummer

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const Str = require('@supercharge/strings');
 const app = express();
 const server = require('http').createServer(app);
 const  io  = require("socket.io")(server, {
-    cors: {origin: '*',}
+    cors: {origin: 'http://localhost:4200'}
 });;
 //const io = new Server(server);
 const port = 9898;
+const packstueck_nummer = [339987849,313593471,335102294,339987853,338097425,320513280]
 
 
 app.get('/', (req, res) => {
@@ -15,12 +16,12 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     setInterval(()=>{
-        io.sockets.emit('broadcast', Str.random() );
+        let randomIndex = Math.floor(Math.random()*packstueck_nummer.length);
+        io.sockets.emit('broadcast', packstueck_nummer[randomIndex] );
     }, 3 * 1000);
 });
 
 
 
 server.listen(port, () => console.log((new Date()) + 'Server is running on port ' + port + ' ...'));
-
 
